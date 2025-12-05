bb13fbc562f8e8fb9b050e82d16801b2c69aac75
C P <13357398-ChriPar@users.noreply.gitlab.com>
2025-12-05 16:26:55 +0700

    Update

--- app/src/main/kotlin/org/fossify/messages/activities/MainActivity.kt
+++ app/src/main/kotlin/org/fossify/messages/activities/MainActivity.kt
@@ -79,7 +79,6 @@ import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
-
 class MainActivity : SimpleActivity() {
     override var isSearchBarEnabled = true
 
