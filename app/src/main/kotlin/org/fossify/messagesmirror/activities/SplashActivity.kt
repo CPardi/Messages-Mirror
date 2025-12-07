8f82b1eb0fe3226bb05bb6e747c972e3cda0fa55
C P <13357398-ChriPar@users.noreply.gitlab.com>
2025-12-07 17:28:18 +0700

    Update

--- app/src/main/kotlin/org/fossify/messagesmirror/activities/SplashActivity.kt
+++ app/src/main/kotlin/org/fossify/messagesmirror/activities/SplashActivity.kt
@@ -5,8 +5,8 @@ import org.fossify.commons.activities.BaseSplashActivity
 import org.fossify.messages.activities.MainActivity
 
 /**
- * Code in org.fossify.commons references classes using the applicationId (Defined in gradle). As the applicationId has been updated, but not the namespace, this
- * class cannot be found. Clone of existing activity in the expected namespace, so that the required fields can be found.
+ * Code in org.fossify.commons references classes using the applicationId (Defined in gradle). As the applicationId has been updated, but not the namespace,
+ * this class cannot be found. Clone of existing activity in the expected namespace, so that the required fields can be found.
  *
  * @see org.fossify.messages.activities.SplashActivity
  */
