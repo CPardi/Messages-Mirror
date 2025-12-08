df859e3e4dacc52d1e4bf12d95263031d0d634c0
C P <13357398-ChriPar@users.noreply.gitlab.com>
2025-12-08 21:30:36 +0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -138,7 +138,7 @@ detekt {
 }
 
 dependencies {
-    implementation(libs.fossify.commons)
+    implementation("com.github.CPardi:Fossify-Commons:0198559c")
     implementation(libs.eventbus)
     implementation(libs.indicator.fast.scroll)
     implementation(libs.mmslib)
