248f16ae444104b9d1205eeff5c6a719c1ba5a57
CPardi <CPardi@users.noreply.github.com>
2025-12-09 17:27:21 +0700

    Use forked fossify.commons and update applicationID (#27)
     - Use fossify.common built by jitpack.io
     - Update SplashActivity aliases as they are resolved from the applicationID
     - Remove unneeded duplicate SplashActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -138,7 +138,7 @@ detekt {
 }
 
 dependencies {
-    implementation(libs.fossify.commons)
+    implementation("com.github.CPardi:Fossify-Commons:3c309b1")
     implementation(libs.eventbus)
     implementation(libs.indicator.fast.scroll)
     implementation(libs.mmslib)
